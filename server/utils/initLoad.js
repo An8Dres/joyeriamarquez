e84b9addc3e04a1996604c174c53e4845ce345b6
Andres Buelvas <130523153+An8Dres@users.noreply.github.com>
2026-07-21 02:28:37 -0500

    fixed fill_db

--- server/utils/initLoad.js
+++ server/utils/initLoad.js
@@ -17,21 +17,20 @@ export async function getPopular() {
 }
 
 async function fillDB() {
-  console.log('fillDB inicializado')
   try {
     const [{count}] = await sql`SELECT COUNT(*) AS count FROM productos`
-    console.log(count)
-    if (count === 0 || count == 0) {
+
+    if (count == 0) {
       const { readFile } = await import('fs/promises')
     
-      const prod = await readFile('./db.json', 'utf-8')
+      const prod = await readFile(process.cwd() + '/server/utils/db.json', 'utf-8')
   
       const productos = JSON.parse(prod)
       console.log(productos)
       for (const p of productos) {
         await sql`INSERT INTO productos (titulo, info, main_image_id, precio, precio_anterior, tipo, stock) VALUES (${p.titulo}, ${p.info}, ${p.main_image_id}, ${p.precio}, ${p.precio_anterior}, ${p.tipo}, ${p.stock})`
       }
-    } else console.log('La base está llena')
+    }
   } catch(e) { console.error(e) }
 }
 
