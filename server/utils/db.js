56889e805a0fa424d14d3d7b624123822a8a940d
Andres Buelvas <130523153+An8Dres@users.noreply.github.com>
2026-07-21 01:47:34 -0500

    Prepare Render

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,16 +1,6 @@
-import dotenv from 'dotenv'
 import postgres from 'postgres'
 
-dotenv.config()
-
-const sql = postgres({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  max: 5
-})
+const sql = postgres(process.env.DB_URL)
 
 try {
   const result = await sql`SELECT NOW()`
--- server/utils/initLoad.js
+++ server/utils/initLoad.js
@@ -15,3 +15,21 @@ export async function getPopular() {
   Format.productParser(data)
   return tplPopulares(data)
 }
+
+async function fillDB() {
+  const [{count}] = await sql`SELECT COUNT(*) AS count FROM productos`
+  
+  if (count === 0) {
+    const { readFile } = await import('fs/promises')
+    
+    const prod = await readFile('./db.json', 'utf-8')
+    
+    const productos = JSON.parse(prod)
+    
+    for (const p of productos) {
+      await sql`INSERT INTO productos (titulo, info, main_image_id, precio, precio_anterior, tipo, stock) VALUES (${p.titulo}, ${p.info}, ${p.main_image_id}, ${p.precio}, ${p.precio_anterior}, ${p.tipo}, ${p.stock})`
+    }
+  }
+}
+
+await fillDB()
