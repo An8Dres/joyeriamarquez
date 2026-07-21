1a86744b21d83312f9686a9bb0d79e8402c929a0
Andres Buelvas <130523153+An8Dres@users.noreply.github.com>
2026-07-21 02:06:49 -0500

    Update fillDb for Render

--- server/utils/initLoad.js
+++ server/utils/initLoad.js
@@ -17,19 +17,21 @@ export async function getPopular() {
 }
 
 async function fillDB() {
-  const [{count}] = await sql`SELECT COUNT(*) AS count FROM productos`
+  try {
+    const [{count}] = await sql`SELECT COUNT(*) AS count FROM productos`
   
-  if (count === 0) {
-    const { readFile } = await import('fs/promises')
+    if (count === 0) {
+      const { readFile } = await import('fs/promises')
     
-    const prod = await readFile('./db.json', 'utf-8')
+      const prod = await readFile('./db.json', 'utf-8')
     
-    const productos = JSON.parse(prod)
+      const productos = JSON.parse(prod)
     
-    for (const p of productos) {
-      await sql`INSERT INTO productos (titulo, info, main_image_id, precio, precio_anterior, tipo, stock) VALUES (${p.titulo}, ${p.info}, ${p.main_image_id}, ${p.precio}, ${p.precio_anterior}, ${p.tipo}, ${p.stock})`
+      for (const p of productos) {
+        await sql`INSERT INTO productos (titulo, info, main_image_id, precio, precio_anterior, tipo, stock) VALUES (${p.titulo}, ${p.info}, ${p.main_image_id}, ${p.precio}, ${p.precio_anterior}, ${p.tipo}, ${p.stock})`
+      }
     }
-  }
+  } catch(e) { console.error(e) }
 }
 
 await fillDB()
